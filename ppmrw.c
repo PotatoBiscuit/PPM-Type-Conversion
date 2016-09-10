c915cc7b7ff85b4e713ca026f728a35a6f1d9855
Erik <eld66@nau.edu>
2016-09-10 15:36:59 -0700

    Basic Program Structure, Started Error handling

--- ppmrw.c
+++ ppmrw.c
@@ -2,15 +2,23 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-	/*my first program in C*/
-	if (argc != 4){ /*If the user tries to put in too many arguments, slap their wrist*/
-		printf("Error: Incorrect amount of arguments");
+	
+	if (argc != 4){ /*If the user tries to put in too many/too little arguments, slap their wrist*/
+		fprintf(stderr, "Error: Incorrect amount of arguments");
 		return 1;
 	}
-	printf("%s", argv[1]);
-	printf("\n");
-	printf("%s", argv[2]);
-	printf("\n");
-	printf("%s", argv[3]);
-	return 0;
+	
+	if (*argv[1] == '3'){	/*If the user types a '3' in the command line, we must convert to P3 PPM format*/
+		printf("Converting to P3 format...\n\n");
+		return 0;
+	}
+	
+	if(*argv[1] == '6'){	/*If the user types a '6' in the command line, we must convert to P6 PPM format*/
+		printf("converting to P6 format...\n\n");
+		return 0;
+	}
+	
+	/*If neither a 3 nor 6 were entered in the command line, the program was used incorrectly*/
+	fprintf(stderr, "\nYou did not provide valid command line arguments\nRemember: ppmrw 3/6(Type of PPM file) input.ppm output.ppm");
+	return 1;
 }
