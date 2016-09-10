055a61284b7fcb6190fb403416608c2280c3599d
Erik <eld66@nau.edu>
2016-09-10 15:06:39 -0700

    Basic C IO test
    I just wanted to get familiar with C's command line input/output
    workings

--- ppmrw.c
+++ ppmrw.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
- int main(){
-	 /*my first program in C*/
-	 printf("Hello, World! \n");
-	 return 0;
- }
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+	/*my first program in C*/
+	if (argc != 4){ /*If the user tries to put in too many arguments, slap their wrist*/
+		printf("Error: Incorrect amount of arguments");
+		return 1;
+	}
+	printf("%s", argv[1]);
+	printf("\n");
+	printf("%s", argv[2]);
+	printf("\n");
+	printf("%s", argv[3]);
+	return 0;
+}
