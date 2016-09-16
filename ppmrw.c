10790c6bb7a781c1c76e7538a91a16589a9d1ce5
Erik <eld66@nau.edu>
2016-09-16 00:54:56 -0700

    Added some Error Catching

--- ppmrw.c
+++ ppmrw.c
@@ -1,10 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 int main(int argc, char *argv[]){
 	
+	FILE * testOpen;
+	char* periodPointer;
+	const char delim[2] = ".";
+	const char extension[4] = "ppm";
+	
 	if (argc != 4){ /*If the user tries to put in too many/too little arguments, slap their wrist*/
-		fprintf(stderr, "Error: Incorrect amount of arguments");
+		fprintf(stderr, "\n\nError: Incorrect amount of arguments\nRemember: ppmrw [3/6](Type of PPM file) input.ppm output.ppm\n\n");
+		return 1;
+	}
+	
+	if(sizeof(argv[1]) != 8){
+		fprintf(stderr, "\n\nError: Second argument must be a single digit number [3/6]\nRemember: ppmrw [3/6](Type of PPM file) input.ppm output.ppm\n\n");
+		return 1;
+	}
+	if(!isdigit(*argv[1])){
+		fprintf(stderr, "\n\nError: Second argument must be a single digit number [3/6]\nRemember: ppmrw [3/6](Type of PPM file) input.ppm output.ppm\n\n");
+		return 1;
+	}
+	
+	if((testOpen = fopen(argv[2], "rb")) == NULL){
+		fprintf(stderr, "\n\nError: Input file must exist\n\n");
+		return 1;
+	}
+	fclose(testOpen);
+	
+	periodPointer = strtok(argv[2], delim);
+	periodPointer = strtok(NULL, delim);
+	if(strcmp(periodPointer, extension) != 0){
+		fprintf(stderr, "\n\nError: Input file must be .ppm format\nRemember: ppmrw [3/6](Type of PPM file) input.ppm output.ppm\n\n");
+		return 1;
+	}
+	
+	periodPointer = strtok(argv[3], delim);
+	periodPointer = strtok(NULL, delim);
+	if(strcmp(periodPointer, extension) != 0){
+		fprintf(stderr, "\n\nError: Output file must be .ppm format\nRemember: ppmrw [3/6](Type of PPM file) input.ppm output.ppm\n\n");
 		return 1;
 	}
 	
@@ -14,13 +50,13 @@ int main(int argc, char *argv[]){
 			return 1;
 		return 0;
 	}
-	
 	if(*argv[1] == '6'){	/*If the user types a '6' in the command line, we must convert to P6 PPM format*/
 		printf("converting to P6 format...\n\n");
 		if(ppmConversionHandler(argv[2], argv[3], 6))
 			return 1;
 		return 0;
 	}
+	
 	
 	/*If neither a 3 nor 6 was entered in the command line, the program was used incorrectly*/
 	fprintf(stderr, "\nYou did not provide valid command line arguments\nRemember: ppmrw [3/6](Type of PPM file) input.ppm output.ppm");
@@ -32,55 +68,115 @@ int ppmConversionHandler(char *input, char *output, int conversionType){	/*This 
 	int filePointerCounter = 0;		/*This will help us gather the height and width file info*/
 	int numDigitsWidth = 0;			/*This will be the digit count of the file Width field*/
 	int numDigitsHeight = 0;		/*This will be the digit count of the file Height field*/
+	int numDigitsAlpha = 0;
 	char *widthPointer;				/*This will point to the Width string we pull from the file*/
 	char *heightPointer;			/*This will point to the Width string we pull from the file*/
+	char *alphaPointer;
 	int width;						/*This is the numeric Width value*/
 	int height;						/*This is the numeric Height value*/
 	char ppmType;					/*Type of PPM file that we need to convert*/
 	FILE *inputPointer = fopen(input, "rb");	/*Open the input file*/
 	
-	
-	fseek(inputPointer, 1, SEEK_SET);		/*Move file pointer one byte to the right, we want the number after the P*/
+	/*What?!*/
+	if(fgetc(inputPointer) != 'P'){	/*Move file pointer one byte to the right, we want the number after the P*/
+		fprintf(stderr, "Error: Header is not in the correct format");
+		return 1;
+	}
 	ppmType = fgetc(inputPointer);			/*Grab the format number of file, which should be 3 or 6*/
+	if(ppmType != '3' && ppmType != '6'){
+		fprintf(stderr, "Error: Sorry, but this software only converts P3 and P6 ppm files");
+		return 1;
+	}
 
-	fseek(inputPointer, 1, SEEK_CUR);		/*Advance file pointer one byte*/
+	if(!isspace(fgetc(inputPointer))){		/*Advance file pointer one byte*/
+		fprintf(stderr, "Error: Header is not in the correct format");
+		return 1;
+	}
 
+	
 	while(!isspace(fgetc(inputPointer))){	/*Iterate through the Width field*/
+		if(filePointerCounter >= 10){
+			fprintf(stderr, "Error: Width field is limited to 10 digits");
+			return 1;
+		}
 		filePointerCounter++;
 	}
 
-	widthPointer = malloc(sizeof(char)*filePointerCounter);		/*Create space for the Width field on this computer*/
+	widthPointer = malloc(sizeof(char)*(filePointerCounter + 1));		/*Create space for the Width field on this computer*/
 	numDigitsWidth = filePointerCounter;						/*Store the digit amount of the Width field*/
 
 	
 	fseek(inputPointer, -1 - filePointerCounter, SEEK_CUR);		/*Move pointer back to the start of the Width field*/
 	while(filePointerCounter){									/*Record each digit of the Width field*/
 		*widthPointer = fgetc(inputPointer);
+		if(!isdigit(*widthPointer)){
+			fprintf(stderr, "Error: Width field contains something other than a number");
+			return 1;
+		}
 		filePointerCounter--;
 		widthPointer++;
 	}
+	*widthPointer = '\0';
 	fseek(inputPointer, 1, SEEK_CUR);							/*Move file pointer to the start of the Height field*/
 	widthPointer -= numDigitsWidth;								/*Move pointer pointing to Width info, back to its origin*/
 
 	
 	while(!isspace(fgetc(inputPointer))){						/*Do what we did for the Width field, for the Height field*/
+		if(filePointerCounter >= 10){
+			fprintf(stderr, "Error: Height field is limited to 10 digits");
+			return 1;
+		}
 		filePointerCounter++;
 	}
-	heightPointer = malloc(sizeof(char)*filePointerCounter);
+	heightPointer = malloc(sizeof(char)*(filePointerCounter + 1));
 	numDigitsHeight = filePointerCounter;
 	fseek(inputPointer, -1 - filePointerCounter, SEEK_CUR);
 
 	
 	while(filePointerCounter){
 		*heightPointer = fgetc(inputPointer);
+		if(!isdigit(*heightPointer)){
+			fprintf(stderr, "Error: Height field contains something other than a number");
+			return 1;
+		}
 		filePointerCounter--;
 		heightPointer++;
 	}
+	*heightPointer = '\0';
 	heightPointer -= numDigitsHeight;
 	
 	
-	fclose(inputPointer);			/*Close input file*/
+	fseek(inputPointer, 1, SEEK_CUR);
+	while(!isspace(fgetc(inputPointer))){
+		if(filePointerCounter >= 3){
+			fprintf(stderr, "Error: Max alpha field is limited to 3 digits");
+			return 1;
+		}
+		filePointerCounter++;
+	}
+	alphaPointer = malloc(sizeof(char)*(filePointerCounter + 1));
+	numDigitsAlpha = filePointerCounter;
+	fseek(inputPointer, -1 - filePointerCounter, SEEK_CUR);
 	
+	while(filePointerCounter){
+		*alphaPointer = fgetc(inputPointer);
+		if(!isdigit(*alphaPointer)){
+			fprintf(stderr, "Error: Max alpha field contains something other than a number");
+			return 1;
+		}
+		filePointerCounter--;
+		alphaPointer++;
+	}
+	*alphaPointer = '\0';
+	alphaPointer -= numDigitsAlpha;
+
+	if(atoi(alphaPointer) != 255){
+		fprintf(stderr, "Error: Files must be in one byte format");
+		return 1;
+	}
+	
+	
+	fclose(inputPointer);			/*Close input file*/
 	width = atoi(widthPointer);
 	height = atoi(heightPointer);
 	numBytes = 3 * width * height;	/*Computer number of bytes in our image using our recorded Height and Width fields*/
@@ -244,7 +340,7 @@ int p3toP6(char *input, char *output, int numBytes){
 			filePointerCounter++;
 		}
 
-		valuePointer = malloc(sizeof(char)*filePointerCounter + 1);		/*Create space for the color value on this computer*/
+		valuePointer = malloc(sizeof(char)*(filePointerCounter + 1));		/*Create space for the color value on this computer*/
 		numDigitsValue = filePointerCounter;						/*Store the digit amount of the color value*/
 
 		
@@ -266,7 +362,7 @@ int p3toP6(char *input, char *output, int numBytes){
 		numBytes--;
 	}
 	buffer -= (numBytes1);
-	fwrite(buffer, 1, numBytes1, outputPointer);
+	fwrite(buffer, sizeof(char), numBytes1, outputPointer);
 	
 	
 	
@@ -291,7 +387,7 @@ int p6toP6(char *input, char *output, int numBytes){
 	}
 	
 	sizeOfRead = fread(buffer, 1, numBytes, inputPointer);	/*Read non-header info from the input file into our buffer*/
-	fwrite(buffer, 1, sizeOfRead, outputPointer);	/*Write the data from our buffer into the output file*/
+	fwrite(buffer, sizeof(char), sizeOfRead, outputPointer);	/*Write the data from our buffer into the output file*/
 	
 	free(buffer);
 	fclose(inputPointer);	/*Close input file*/
